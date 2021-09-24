d4842f82334c6055f1b5ed99133bb0aecbdcef63
MS2104 <31002@ma-web.nl>
2021-09-24 10:38:23 +0200

    Opdracht af

---
+++ PYTB1L2FamilyTies/wiebenik.py
@@ -0,0 +1,10 @@
+naam = ''
+leeftijd = 16
+woonplaats = 'amsterdam'
+afstand = 20
+broer = 1
+zus = 1
+
+print('Mijn naam is ', naam, ', ik ben ', leeftijd, ' jaar oud en woon in ', woonplaats, '. Ik woon thuis met ', broer, ' broertje die 11 is', ',')
+print(' en ', zus, ' zusje die 13 is. Ik woon thuis met beide mijn ouders.')
+print('Ik woon ', afstand, ' kilometeres van school.')
